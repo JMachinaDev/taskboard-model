27f73aa2222947f6580db9f553eb887dfd677cb5
jmachinadev <jmachinadev@gmail.com>
2021-01-08 13:26:50 -0500

    **WIP**

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,54 +1,48 @@
-import React from 'react';
+import React from "react";
 
-const TasksTodoAndTasksDone = (post, index) => {
-  return (
-      <>
-          <main className="main-container">
+const TasksTodoAndTasksDone = (props) => {
+	const { post, index } = props;
+	return (
+		<main className='main-container'>
+			<section className='task-container' key={index}>
+				{/* Tasks ToDo Container*/}
+				<section className='task-todo-container'>
+					<header style={{ backgroundColor: "red" }}>
+						<h2>Tasks</h2>
+					</header>
 
-            <section 
-              className="task-container"
-              key={index}
-              >
-              {/* Tasks ToDo */}
-              <section className="task-todo-container">
-                  <header>
-                      <h2>{post.title}</h2>
-                  </header>
+					<section>
+						<h3>Task: {post.title}</h3>
+						<p>Description: {post.description}</p>
 
-                  <section>
-                      <h3>Task ToDo</h3>
-                      <p>Description</p>
+						<div>
+							<span type='date'>Date Post: {new Date(post.date).toLocaleDateString()}</span>
+							<img alt='' /> {/* author image goes here*/}
+              {/* <p>Posted By: {post.author}</p> */}
+						</div>
 
-                      <div>
-                        <span type="date">Date published</span>
-                        <img alt=''/> {/* author image goes here*/}
-                      </div>
+						<div>Done/Not Done</div>
+						<a href='#!'>Git Commit Link</a>
+					</section>
+				</section>
 
-                      <div>Done/Not Done</div>
-                      <a href="#!" >Git Commit Link</a>
-
-                  </section>
-              </section>
-
-              {/*Tasks Done */}
-              <section className="task-done-container">
-                  <header>
-                      <h2>Tasks Done</h2>
-                  </header>
-                  <section>
-                      {/* where done tasks will be */}
-                      <div>Task 1</div>
-                      <div>Task 1</div>
-                      <div>Task 1</div>
-                      <div>Task 1</div>
-                      <div>Task 1</div>
-                  </section>
-              </section>
-            </section>
-
-          </main>
-      </>
-  )
-}
+				{/*Tasks Done Container*/}
+				<section className='task-done-container'>
+					<header>
+						<h2>Tasks Done</h2>
+					</header>
+					<section>
+						{/* where done tasks will be */}
+						<div>Task 1</div>
+						<div>Task 1</div>
+						<div>Task 1</div>
+						<div>Task 1</div>
+						<div>Task 1</div>
+					</section>
+				</section>
+			</section>
+		</main>
+	);
+};
 
 export default TasksTodoAndTasksDone;
