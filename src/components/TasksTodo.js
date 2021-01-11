2108fcb84808212cfb5141ddab6a9dbfeec57c22
jmachinadev <jmachinadev@gmail.com>
2021-01-11 15:47:12 -0500

    cleaned up

--- src/components/TasksTodo.js
+++ src/components/TasksTodo.js
@@ -1,52 +1,41 @@
 import React from "react";
-// import imageUrlbuilder from '@sanity/image-url';
-
+import urlFor from '../components/ImageBuilder';
 
 const TasksTodo = (props) => {
   const { post, index } = props;
-  
-  // urlFor = (source) => {
-  //   return this.builder.image(source)
-  // };
 
-	return (
-    <main 
-      className='main-container'
+  return (
+    <main
       to={post.slug.current}
       key={post.slug.current}
     >
-      <section 
-        className='task-container' 
+      <section
+        className='task-todo-container'
         key={index}
       >
-				{/* Tasks ToDo Container*/}
-        <section 
-          className='task-todo-container'
-        >
-          
-					<section>
-						<h3>Task: {post.title}</h3>
-						<p>Description: {post.description}</p>
-						<div>
-              <span 
-                type='date'>Date Post: {new Date(post.date).toLocaleDateString()}
-              </span>
-						</div>
-              <p>Posted By: {post.name}</p>
-              <img
-                // src={urlFor(post.authorImage).url()}
-                alt={post.name} 
-              /> {/* author image goes here*/}
-						<div>Done/Not Done</div>
-            <a 
-              href='#!'
-            >
-              Git Commit Link
-            </a>
+          <header>
+            <h3>Task: {post.title}</h3>
+          </header>
+          <p>Description: {post.description}</p>
 
-					</section>
-				</section>
-			</section>
+          <div>
+            <span
+              type='date'>Posted: {new Date(post.date).toLocaleDateString()}
+            </span>
+          </div>
+
+          <div>{/*Red = Not Done, Green = Done*/}</div>
+
+          <span className="image-container">
+            <img
+              src={urlFor(post.authorImage).url()}
+              alt={post.name}
+              className="author-image"
+            />
+            <p>{post.name}</p>
+          </span>
+
+      </section>
 		</main>
 	);
 };
